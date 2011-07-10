6cfc6cea9e5609732be52f6b2330cdbd4a2f530a
shoshi <shoshanaroberts@gmail.com>
2011-07-10 17:51:36 -0400

    User Sign In Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
-	resources :users
-	
-	match '/signup',	:to => 'users#new'
+  # get "sessions/new"
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
-	match '/contact',	:to => 'pages#contact'
-	match '/about',		:to => 'pages#about'
-	match '/help',		:to => 'pages#help'
-	
-	root :to => 'pages#home'
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
 end
