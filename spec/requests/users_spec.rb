dbc2dbff9deb9f576060393a09044c4e020bb6db
shoshi <shoshanaroberts@gmail.com>
2011-07-12 23:42:10 -0400

    Simplifies sign-in integration tests w/ a function

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,10 +9,10 @@ describe "Users" do
       it "should not make a new user" do
         lambda do
           visit signup_path
-          fill_in "Name",			:with => " "
-          fill_in "Email",		:with => " "
-          fill_in "Password",		:with => " "
-          fill_in "Confirmation",	:with => " "
+          fill_in "Name",         :with => " "
+          fill_in "Email",        :with => " "
+          fill_in "Password",     :with => " "
+          fill_in "Confirmation", :with => " "
           click_button
           response.should render_template('users/new')
           response.should have_selector("div#error_explanation")
@@ -25,10 +25,10 @@ describe "Users" do
       it "should make a new user" do
         lambda do
           visit signup_path
-          fill_in "Name",			:with => "Example User"
-          fill_in "Email",		:with => "user@example.com"
-          fill_in "Password",		:with => "foobar"
-          fill_in "Confirmation",	:with => "foobar"
+          fill_in "Name",         :with => "Example User"
+          fill_in "Email",        :with => "user@example.com"
+          fill_in "Password",     :with => "foobar"
+          fill_in "Confirmation", :with => "foobar"
           click_button
           response.should have_selector("div.flash.success", :content => "Welcome")
           response.should render_template('users/show')
@@ -52,10 +52,7 @@ describe "Users" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,     :with => user.email
-        fill_in :password,  :with => user.password
-        click_button
+        integration_sign_in(user)
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
