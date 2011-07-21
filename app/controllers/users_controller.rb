8794b234c4597a03a50c385b063464f69886af19
shoshi <shoshanaroberts@gmail.com>
2011-07-20 23:09:57 -0400

    Users must be signed in to edit/update themselves

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only  =>  [:edit, :update]
+  before_filter :correct_user, :only  =>  [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -26,12 +28,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated!"
       redirect_to @user
@@ -40,4 +40,15 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
