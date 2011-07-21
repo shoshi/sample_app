ea446265fa985d63537ec854e1a76986f9b242b1
shoshi <shoshanaroberts@gmail.com>
2011-07-20 23:21:47 -0400

    Friendly forwarding on sign-in to requested page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,7 +27,13 @@ module SessionsHelper
   end
 
   def deny_access
+    store_location
     redirect_to signin_path, :notice => "Please sign in to access this page"
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
 
   private
@@ -38,5 +44,13 @@ module SessionsHelper
 
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 end
