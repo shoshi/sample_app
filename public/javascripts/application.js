2c5302428da66a97a545b1886ba3e5b77b5333e7
shoshi <shoshanaroberts@gmail.com>
2012-01-21 16:56:07 -0500

    Open Gravatar editing link in a new window

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+// Open Links with `rel="external"` in a new window
+function externalLinks() {
+  var anchors = document.getElementsByTagName("a"),
+      i;
+
+  if (!document.getElementsByTagName){
+    return;
+  }
+  for (i = 0; i<anchors.length; i++) {
+    var anchor = anchors[i];
+    if (anchor.getAttribute("href") && anchor.getAttribute("rel") === "external"){
+      anchor.target = "_blank";
+    }
+  }
+}
+window.onload = externalLinks;
