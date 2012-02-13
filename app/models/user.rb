a5d0276206ae3e97af22ec7650d75ffd8fcc0012
shoshi <shoshanaroberts@gmail.com>
2012-02-12 20:32:52 -0500

    Added following, and unfollowing in suit (12.1.4)

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ActiveRecord::Base
   has_many :microposts, :dependent => :destroy
   has_many :relationships, :foreign_key => "follower_id",
                            :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -48,6 +49,18 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
+  end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
 
   def feed
