0c0156f9145f4afcd6a8bb8c9753e9dd2b1790c9
shoshi <shoshanaroberts@gmail.com>
2012-02-12 20:42:26 -0500

    Add followers through reverse_relationships(12.1.5)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -225,5 +225,18 @@ describe User do
       @user.unfollow!(@followed)
       @user.should_not be_following(@followed)
     end
+
+    it "should have a reverse_relationships method" do
+      @user.should respond_to(:reverse_relationships)
+    end
+
+    it "should have a followers method" do
+      @user.should respond_to(:followers)
+    end
+
+    it "should include the follower in the followers array" do
+      @user.follow!(@followed)
+      @followed.followers.should include(@user)
+    end
   end
 end
