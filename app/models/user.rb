9d2c6a659138be3aa848f3c35bd19ae3b6179ae0
shoshi <shoshanaroberts@gmail.com>
2012-02-12 20:16:15 -0500

    User/relationship associations&validations(12.1.3)

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -190,4 +190,15 @@ describe User do
       end
     end
   end
+
+  describe "relationships" do
+    before(:each) do
+      @user = User.create!(@attr)
+      @followed = Factory(:user)
+    end
+
+    it "should have a relationships method" do
+      @user.should respond_to(:relationships)
+    end
+  end
 end
