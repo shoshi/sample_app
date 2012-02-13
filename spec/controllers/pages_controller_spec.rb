803bd4c191f4fcaf0bed1c7fa80f3decf1177090
shoshi <shoshanaroberts@gmail.com>
2012-02-12 21:20:33 -0500

    User stats & follow form, not hooked up (12.2.2)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,20 +4,41 @@ describe PagesController do
   render_views
 
   before(:each) do
-    #
-    # Define @base_title here.
-    #
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
+
+    describe "when not signed in" do
+
+      before(:each) do
+        get :home
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        response.should have_selector("title",
+                                      :content => "#{@base_title} | Home")
+      end
     end
 
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title" , :content => "Ruby on Rails Tutorial Sample App | Home")
+    describe "when signed in" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector("a", :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector("a", :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end
     end
   end
 
@@ -29,7 +50,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title" , :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Contact")
     end
   end
 
@@ -41,7 +63,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title" , :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | About")
     end
   end
 
@@ -53,7 +76,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title" , :content => "Ruby on Rails Tutorial Sample App | Help")
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Help")
     end
   end
 end
